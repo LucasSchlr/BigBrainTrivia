089956c9de4a8c4e7ad38c1ba27ba2a2cb90c732
Eduardo Arndt <eduardoarndt1@gmail.com>
2020-11-11 00:10:39 -0300

    transform response unicode to utf8

--- app/src/main/java/com/example/bigbraintrivia/data/DatabaseExecutor.kt
+++ app/src/main/java/com/example/bigbraintrivia/data/DatabaseExecutor.kt
@@ -3,22 +3,23 @@ package com.example.bigbraintrivia.data
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.RequestBody.Companion.toRequestBody
+import org.apache.commons.text.StringEscapeUtils
 import java.io.IOException
 
-
 object DatabaseExecutor {
-    private const val path = "http://10.0.2.2/bigbraintrivia/database.php"
+    private const val path = "http://localhost/bigbraintrivia/database.php"
     private val client = OkHttpClient()
 
-    fun executeQuery(query: String) {
+    fun executeQuery(query: String): String {
         val request: Request = Request.Builder()
             .url(path)
             .post(query.toRequestBody())
             .build()
 
         client.newCall(request).execute().use { response ->
+
             if (!response.isSuccessful) throw IOException("Unexpected code $response")
-            println(response.body!!.string())
+            return StringEscapeUtils.unescapeJava(response.body!!.string())
         }
     }
 }
